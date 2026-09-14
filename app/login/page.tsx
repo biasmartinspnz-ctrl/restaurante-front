39ec91aa3a01678f9e22cd794918c999f3238d8b
ALUNO SESI PETROLINA <biasmartinspnz@gmail.com>
2026-09-14 15:01:45 -0300

    ajeitado2

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -50,8 +50,8 @@ export default function Login(){
                 value={senha}
                 onChange={(e)=>setSenha(e.target.value)}
                 placeholder="Digite sua senha"
-                className="w-full rounded-lg border p-3"
-                outline-none focus:ring-2 focus:ring-blue-500
+                className="w-full rounded-lg border p-3
+                outline-none focus:ring-2 focus:ring-blue-500"
 >
                 </input>
             </div>
